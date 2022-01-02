8b363ee7533f8c0fbedeee0dd78353b94c32e5da
Alfie Xiong <2409898240@qq.com>
2022-01-02 17:06:32 +0800

    add sum max sumSpeedUp maxSpeedUp function

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<Windows.h>
 #include"myMath.h"
+#include"omp.h"
 #define MAX_THREADS 64
 #define SUBDATANUM 4000000
 #define DATANUM (SUBDATANUM * MAX_THREADS)
@@ -10,13 +11,11 @@ int main() {
 	LARGE_INTEGER end;
 	float* rawFloatData = new float[DATANUM];
 	for (size_t i = 0; i < DATANUM; i++)
-	{
 		rawFloatData[i] = float(i + 1);
-	}
 	QueryPerformanceCounter(&start);
-	float sum = myMath::sum(rawFloatData, DATANUM);
+	float maxNum = myMath::m_maxSpeedUp(rawFloatData, DATANUM);
 	QueryPerformanceCounter(&end);
-	cout << sum << endl;
+	cout << maxNum << endl;
 	cout << "Time Consumed:" << (end.QuadPart - start.QuadPart) << endl;
 	delete[] rawFloatData;
 	return 0;
--- client/myMath.cpp
+++ client/myMath.cpp
@@ -1,23 +1,27 @@
 #include "myMath.h"
 #include<iostream>
+#include "omp.h"
+#include <immintrin.h>
+#define MAX_THREADS 64
 using namespace std;
 
 float myMath::sum(const float data[], const int len) {
-	float sum = 0;
+	float sumNum = 0;
 	for (int i = 0; i < len; i++) {
-		sum += log(sqrt(data[i]));
+		sumNum += log(sqrt(data[i]));
 	}
-	return sum;
+	return sumNum;
 }
 
 float myMath::m_max(const float data[], const int len) {
-	float min = log(sqrt(data[0]));
+	float maxNum = log(sqrt(data[0]));
 	for (int i = 1; i < len; i++) {
-		if (log(sqrt(data[i])) < min) {
-			min = log(sqrt(data[i]));
+		float temp = log(sqrt(data[i]));
+		if (temp > maxNum) {
+			maxNum = temp;
 		}
 	}
-	return min;
+	return maxNum;
 }
 
 float myMath::sort(const float data[], const int len, float  result[]) {
@@ -25,14 +29,57 @@ float myMath::sort(const float data[], const int len, float  result[]) {
 }
 
 float myMath::sumSpeedUp(const float data[], const int len) {
-	return 0;
+	float sumNum = 0;
+	int iter = len / (MAX_THREADS * 8);  //_m256存储8位浮点数
+	__m256* tempResult = new __m256[MAX_THREADS]; //将数据分为MAX_THREADS块
+#pragma omp parallel for num_threads(MAX_THREADS)
+	for (int i = 0; i < MAX_THREADS; i++) {
+		tempResult[i] = _mm256_setzero_ps();
+		__m256* ptr = (__m256*)(data + i * (len / MAX_THREADS));
+		for (int j = 0; j < iter; j++, ptr++) {
+			tempResult[i] = _mm256_add_ps(tempResult[i], _mm256_log_ps(_mm256_sqrt_ps(*ptr)));
+		}
+	}
+	__m256 localResult = _mm256_setzero_ps();
+	for (int i = 0; i < MAX_THREADS; i++) {
+		localResult = _mm256_add_ps(localResult, tempResult[i]);
+	}
+	float* floatResult = (float*)&localResult;
+	for (int i = 0; i < 8; i++) {
+		sumNum = sumNum + floatResult[i];
+	}
+	delete[] tempResult;
+	return sumNum;
 }
 
 float myMath::m_maxSpeedUp(const float data[], const int len) {
-	return 0;
+	float maxNum = log(sqrt(data[0]));
+
+	int iter = len / (MAX_THREADS * 8);  //_m256存储8位浮点数
+	__m256* tempResult = new __m256[MAX_THREADS]; //将数据分为MAX_THREADS块
+#pragma omp parallel for num_threads(MAX_THREADS)
+	for (int i = 0; i < MAX_THREADS; i++) {
+		tempResult[i] = _mm256_setzero_ps();
+		__m256* ptr = (__m256*)(data + i * (len / MAX_THREADS));
+		for (int j = 0; j < iter; j++, ptr++) {
+			tempResult[i] = _mm256_max_ps(tempResult[i], _mm256_log_ps(_mm256_sqrt_ps(*ptr)));
+		}
+	}
+	__m256 localResult = _mm256_setzero_ps();
+	for (int i = 0; i < MAX_THREADS; i++) {
+		localResult = _mm256_max_ps(localResult, tempResult[i]);
+	}
+	float* floatResult = (float*)&localResult;
+	for (int i = 0; i < 8; i++) {
+		if (maxNum < floatResult[i])
+			maxNum = floatResult[i];
+	}
+	delete[] tempResult;
+	return maxNum;
 }
 
 float myMath::sortSpeedUp(const float data[], const int len, float  result[]) {
 	return 0;
 }
+
 
